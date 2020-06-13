99187556c7ee55276f065ccb6cb7e0e29f2ecb2a
Ritresh Girdhar <ritgirdh@publicisgroupe.net>
2020-06-13 23:58:12 +0530

    Terraform changes

--- README.md
+++ README.md
@@ -1,1 +1,69 @@
 # Terraform gcp scheduler cloudrun
+
+This application will help you in creating gcp cloud run service via terraform. 
+
+### Let's build microservice image
+
+```
+$ mvn clean install -f weather-service/pom.xml
+```
+
+### Tag and Push image
+```
+$ docker tag sample-microservice/weather-service gcr.io/api-project-808184727589/ms1
+$ docker push gcr.io/api-project-808184727589/ms1
+```
+
+### Terraform resource - Cloud Run  
+```
+resource "google_cloud_run_service" "default" {
+  name     = "cloudrun-srv"
+  location = "us-central1"
+  template {
+    spec {
+      containers {
+        image = "gcr.io/api-project-808184727589/ms1"
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
+```
+It will create cloud run service of ms1 running container will start serving 100% traffic.  
+
+
+### Create Scheduler job 
+````
+resource "google_cloud_scheduler_job" "job" {
+  name             = "report-generation-job"
+  description      = "report generation job"
+  schedule         = "*/2 * * * *"
+  time_zone        = "America/New_York"
+  attempt_deadline = "320s"
+
+  retry_config {
+    retry_count = 1
+  }
+
+  http_target {
+    http_method = "GET"
+    uri         = "https://example.com/ping"
+  }
+}
+````
+
+
+#### Initialize 
+```
+terraform init
+```
+
+#### Apply changes.
+```
+terraform apply
+```
+
--- main.tf
+++ main.tf
@@ -6,6 +6,13 @@ provider "google" {
   zone    = "us-central1-c"
 }
 
+//data "google_container_registry_repository" "weather-service" {
+//}
+//
+//output "gcr_location" {
+//  value = data.google_container_registry_repository.weather-service.repository_url
+//}
+
 resource "google_cloud_run_service" "default" {
   name     = "cloudrun-srv"
   location = "us-central1"
@@ -22,4 +29,41 @@ resource "google_cloud_run_service" "default" {
     percent         = 100
     latest_revision = true
   }
+}
+
+output "url" {
+  value = "${google_cloud_run_service.default.status[0].url}"
+}
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "policy" {
+  location    = google_cloud_run_service.default.location
+  project     = google_cloud_run_service.default.project
+  service     = google_cloud_run_service.default.name
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
+
+resource "google_cloud_scheduler_job" "job" {
+  name             = "test-job"
+  description      = "test http job"
+  schedule         = "* */8 * * *"
+  time_zone        = "America/New_York"
+
+  retry_config {
+    retry_count = 1
+  }
+
+  http_target {
+    http_method = "GET"
+    uri         = "${google_cloud_run_service.default.status[0].url}/v1/weather/hello1"
+  }
+
 }
